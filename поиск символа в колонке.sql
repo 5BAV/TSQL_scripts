e4e169e273f08955da0114c0236d041bb25af40f
BAV <5bav@mail.ru>
2024-08-28 13:13:33 +0300

    добавил:
    -поиск символа в колонке.sql
    -уникальные символы в колонке (алфавит поля).sql

---
+++ поиск символа в колонке.sql
@@ -0,0 +1,27 @@
+﻿declare
+	@table sysname = 'dbo.[any table]'
+	,@column sysname = '[any field]'
+	,@symbols sysname = 'char(10), char(13), char(9)'
+	
+declare	@query varchar(max) = '
+	;with ten as (select * from (values (0),(0),(0),(0),(0),(0),(0),(0),(0),(0)) as t(n))
+	,ord as (select row_number() over(order by (select null)) as N from ten as t1, ten as t2, ten as t3) --1000
+	select 
+		t.' + @column + '
+		,string_agg(concat(unicode(o.[char]), ''['', o.N, '']''), '', '') within group (order by o.N) as [unicode (position)]
+	from
+		' + @table + ' as t with(nolock)
+		cross apply (
+			select top(len(t.' + @column + '))
+				substring(t.' + @column + ', o.n, 1) as [char]
+				,o.N
+			from ord as o
+			order by o.N
+		) as o
+	where o.[char] in (' + @symbols + ')
+	group by t.' + @column + '
+	order by t.' + @column + '
+	option (maxdop 8)
+'
+exec (@query)
+
---
+++ уникальные символы в колонке (алфавит поля).sql
@@ -0,0 +1,19 @@
+﻿;with ten as (select * from (values (0),(0),(0),(0),(0),(0),(0),(0),(0),(0)) as t(n))
+,ord as (select row_number() over(order by (select null)) as N from ten as t1, ten as t2, ten as t3, ten as t4) --10000
+select
+	o.[char]
+	,count(*) as [count]
+from
+	[any table] as t with(nolock)
+	cross apply (
+		select
+			substring(t.[any field], o.N, 1) as [char]
+			,o.N
+		from ord as o
+		order by o.N
+		offset 0 rows fetch next len(t.[any field]) rows only
+	) as o
+where o.[char] not in ('0','1','2','3','4','5','6','7','8','9', char(32))
+group by o.[char]
+order by 2, 1
+option (maxdop 8)
